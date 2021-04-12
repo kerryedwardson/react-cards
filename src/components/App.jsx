49cf57872db123f537c90b226bd7c92d6c228a99
Kerry Edwardson <57094218+kerryedwardson@users.noreply.github.com>
2021-04-12 17:51:09 +1000

    use contacts file to access contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import Card from "./Card";
+import Contacts from "../contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card
-        name="Beyonce"
-        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        tel="+123 456 789"
-        email="b@beyonce.com"
+        name={Contacts[0].name}
+        img={Contacts[0].imgURL}
+        tel={Contacts[0].phone}
+        email={Contacts[0].email}
       />
       <Card
-        name="Jack Bauer"
-        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-        tel="+987 654 321"
-        email="jack@nowhere.com"
+        name={Contacts[1].name}
+        img={Contacts[1].imgURL}
+        tel={Contacts[1].phone}
+        email={Contacts[1].email}
       />
       <Card
-        name="Chuck Norris"
-        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-        tel="+918 372 574"
-        email="gmail@chucknorris.com"
+        name={Contacts[2].name}
+        img={Contacts[2].imgURL}
+        tel={Contacts[2].phone}
+        email={Contacts[2].email}
       />
     </div>
   );
